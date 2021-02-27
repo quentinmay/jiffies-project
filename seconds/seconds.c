38bab015618120c61608a8c405705cddd6ccf385
Quentin May <quentinemay@gmail.com>
2021-02-26 18:12:30 -0800

    Initial Commit

---
+++ seconds/seconds.c
@@ -0,0 +1,66 @@
+/**
+ * seconds.c
+ *
+ * Kernel module that communicates with /proc file system.
+ *
+ * Operating System Concepts - 10th Edition
+ * Copyright John Wiley & Sons - 2018
+ */
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/proc_fs.h>
+#include <asm/uaccess.h>
+#include <linux/hash.h>
+#include <linux/jiffies.h>
+#define BUFFER_SIZE 128
+#define PROC_NAME "seconds"
+/**
+ * Function prototypes
+ */
+ssize_t proc_read(struct file *file, char *buf, size_t count, loff_t *pos);
+
+static struct file_operations proc_ops = {
+        .owner = THIS_MODULE,
+        .read = proc_read,
+};
+/* This function is called when the module is loaded. */
+int proc_init(void) {
+        // creates the /proc/hello entry
+        // the following function call is a wrapper for
+        // proc_create_data() passing NULL as the last argument
+        proc_create(PROC_NAME, 0, NULL, &proc_ops);
+        printk(KERN_INFO "/proc/%s created\n", PROC_NAME);
+        
+    return 0;
+}
+/* This function is called when the module is removed. */
+void proc_exit(void) {
+        int i;
+        // removes the /proc/seconds entry
+        remove_proc_entry(PROC_NAME, NULL);
+
+        printk( KERN_INFO "/proc/%s removed\n", PROC_NAME);
+}
+
+ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, loff_t *pos) {
+        int rv = 0;
+        char buffer[BUFFER_SIZE];
+        static int completed = 0;
+        if (completed) {
+                completed = 0;
+                return 0;
+        }
+        completed = 1;
+        long seconds = jiffies/HZ;
+        rv = sprintf(buffer, "Seconds is: %lu\n", seconds);
+        // copies the contents of buffer to userspace usr_buf
+        copy_to_user(usr_buf, buffer, rv);
+        return rv;
+}
+/* Macros for registering module entry and exit points. */
+module_init( proc_init );
+module_exit( proc_exit );
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Seconds Display Module");
+MODULE_AUTHOR("SGG");
